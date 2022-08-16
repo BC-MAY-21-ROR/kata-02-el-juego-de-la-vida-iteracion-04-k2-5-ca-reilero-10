8a75bb6c68c96e7bc6b5e54c8fd3886f69c642e4
Daniel Preciado <fpreciado3@ucol.mx>
2022-08-16 18:57:51 -0500

    create test for the cell class to verify its life status

--- app_spec_rspec.rb
+++ app_spec_rspec.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 require 'rspec'
-require_relative './app'
+require_relative './app.rb'
+
+
+describe Cell do 
+    describe '#initialize' do
+        it 'verifies the life status of a cell' do
+            expect(Cell.new(:alive, 5, 2).to_s).to eq('*')
+        end
+    end
+end
