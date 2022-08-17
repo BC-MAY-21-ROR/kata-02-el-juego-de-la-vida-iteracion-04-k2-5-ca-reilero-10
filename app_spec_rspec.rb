391382ff456a96399b041c5490ee0b1c2c44d10a
GustavoHiguera <ghiguera@ucol.mx>
2022-08-16 19:01:23 -0500

    add dead cell test

--- app_spec_rspec.rb
+++ app_spec_rspec.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 require 'rspec'
-require_relative './app.rb'
+require_relative './app'
 
-
-describe Cell do 
-    describe '#initialize' do
-        it 'verifies the life status of a cell' do
-            expect(Cell.new(:alive, 5, 2).to_s).to eq('*')
-        end
+describe Cell do
+  describe '#initialize' do
+    it 'verifies that the alive cell is represented by an asterisk' do
+      expect(Cell.new(:alive, 5, 2).to_s).to eq('*')
     end
+    it 'verifies that the dead cell is represented by a dot' do
+      expect(Cell.new(:dead, 5, 2).to_s).to eq('.')
+    end
+  end
 end
